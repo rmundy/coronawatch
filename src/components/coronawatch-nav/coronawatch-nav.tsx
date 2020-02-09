cb1740888f1e5311401098b85677f08e25dcfb75
Bobby Mundy <robert.l.mundy@gmail.com>
2020-02-09 12:38:29 -0500

    added style updates

--- src/components/coronawatch-nav/coronawatch-nav.tsx
+++ src/components/coronawatch-nav/coronawatch-nav.tsx
@@ -1,32 +1,29 @@
-import { Component, Host, Prop, State, h } from "@stencil/core";
-import { Store, Unsubscribe } from "@stencil/redux";
+import { Component, Host, Prop, State, h } from '@stencil/core';
+import { Store, Unsubscribe } from '@stencil/redux';
 
 @Component({
-  tag: "app-coronawatch-nav",
-  styleUrl: "coronawatch-nav.scss",
+  tag: 'app-coronawatch-nav',
+  styleUrl: 'coronawatch-nav.scss',
   shadow: true
 })
 export class CoronawatchNav {
   storeUnsubscribe: Unsubscribe;
 
   @State()
-  name: AppState["page"]["name"];
+  name: AppState['page']['name'];
 
-  @Prop({ context: "store" })
+  @Prop({ context: 'store' })
   store: Store;
 
   componentWillLoad() {
-    this.storeUnsubscribe = this.store.mapStateToProps(
-      this,
-      (state: AppState) => {
-        const {
-          page: { name }
-        } = state;
-        return {
-          name
-        };
-      }
-    );
+    this.storeUnsubscribe = this.store.mapStateToProps(this, (state: AppState) => {
+      const {
+        page: { name }
+      } = state;
+      return {
+        name
+      };
+    });
   }
 
   componentDidUnload() {
@@ -38,8 +35,20 @@ export class CoronawatchNav {
       <Host>
         <slot>
           <div class="coronawatch-nav__wrapper">
-            <div class="coronawatch-nav__default">CoronaVirus Facts</div>
-            <div class="coronawatch-nav__back">Back</div>
+            <div class="coronawatch-nav__default">
+              <span>Coronavirus Facts</span>
+              <img
+                class="coronawatch-nav__default__img"
+                src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIsMEExMiwxMiwwLDEsMCwyNCwxMiwxMiwxMiwwLDAsMCwxMiwwWk0xMC40MzIsMTgsOS4wMTgsMTYuNTksMTMuNTkyLDEyLDkuMDE4LDcuNDIxbDEuNDE0LTEuNDE2TDE2LjQyLDEybC01Ljk4OCw2LjAwNVoiIGZpbGw9IiNmZmYiLz48L3N2Zz4="
+              ></img>
+            </div>
+            <div class="coronawatch-nav__back">
+              <span>Back</span>
+              <img
+                class="coronawatch-nav__back__img"
+                src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIsMEExMiwxMiwwLDEsMCwyNCwxMiwxMiwxMiwwLDAsMCwxMiwwWk0xMC40MzIsMTgsOS4wMTgsMTYuNTksMTMuNTkyLDEyLDkuMDE4LDcuNDIxbDEuNDE0LTEuNDE2TDE2LjQyLDEybC01Ljk4OCw2LjAwNVoiIGZpbGw9IiNmZmYiLz48L3N2Zz4="
+              ></img>
+            </div>
           </div>
         </slot>
       </Host>
